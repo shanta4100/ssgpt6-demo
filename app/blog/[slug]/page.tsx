608aa9efb17b649b3dbd872b97294f9d58f1a8a4
Arifur Shanta <p888qd7jc8@privaterelay.appleid.com>
2026-04-16 03:00:54 -0400

    Create page.tsx

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import { notFound } from 'next/navigation'
+import { getPostBySlug } from '@/lib/posts'
+ 
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const post = getPostBySlug(slug)
+ 
+  if (!post) {
+    notFound()
+  }
+ 
+  return <div>{post.title}</div>
+}
