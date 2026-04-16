c7d855eb414db655725f3bf823c0bb31c5443a78
Arifur Shanta <p888qd7jc8@privaterelay.appleid.com>
2026-04-16 02:58:55 -0400

    Create actions.ts

---
+++ app/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+ 
+export async function createPost(prevState: any, formData: FormData) {
+  const title = formData.get('title')
+  const content = formData.get('content')
+ 
+  const res = await fetch('https://api.vercel.app/posts', {
+    method: 'POST',
+    body: { title, content },
+  })
+  const json = await res.json()
+ 
+  if (!res.ok) {
+    return { message: 'Failed to create post' }
+  }
+}
