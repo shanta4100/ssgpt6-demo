31fe8a5c4a60b7597ea52906b2cb6d082a221b9b
Arifur Shanta <p888qd7jc8@privaterelay.appleid.com>
2026-04-16 04:12:46 -0400

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,7 @@
-import { notFound } from 'next/navigation'
-import { getPostBySlug } from '@/lib/posts'
- 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string }>
-}) {
-  const { slug } = await params
-  const post = getPostBySlug(slug)
- 
-  if (!post) {
-    notFound()
-  }
- 
-  return <div>{post.title}</div>
+export async function getPostBySlug(_slug: string) {
+  return null;
+}
+
+export async function getAllPosts() {
+  return [];
 }
